187fa6922b09f0588f1b4c6de1ac367916e3f3f4
destructodroid <omkaryel@gmail.com>
2023-04-04 23:57:56 +0530

    Anima Updated

--- Assets/Anima.cs
+++ Assets/Anima.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(SpriteRenderer))]
 public class Anima : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+   
+
+    public Sprite[] sprites;
+    public float animationTime = 0.25f;
+
+    private SpriteRenderer spriteRenderer;
+    private float timer;
+    private int currentIndex;
+
+    private void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprites[0];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer < animationTime)
+        {
+            return;
+        }
+
+        spriteRenderer.sprite = sprites[currentIndex++ % sprites.Length];
+        timer = 0f;
     }
 }
+
